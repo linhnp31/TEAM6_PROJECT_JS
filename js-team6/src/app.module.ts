b969c85cf370f998f94a15f827da19b162051a1e
nmh14022005 <158242591+nmh14022005@users.noreply.github.com>
2024-03-21 18:37:21 +0700

    Update app.module.ts

--- js-team6/src/app.module.ts
+++ js-team6/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './modules/products/product.modules';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
@@ -9,7 +8,7 @@ import { dataSourceOptions } from 'db/data-source';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, AuthModule, TypeOrmModule.forRoot(dataSourceOptions), UserModule,ConfigModule.forRoot()], 
+  imports: [ AuthModule, TypeOrmModule.forRoot(dataSourceOptions), UserModule,ConfigModule.forRoot()], 
   controllers: [AppController],
   providers: [AppService],
 })
