18d9ff3d88e20a7cea69edbdac3b17bf54500603
nmh14022005 <158242591+nmh14022005@users.noreply.github.com>
2024-03-21 18:35:29 +0700

    Update user.module.ts

--- js-team6/src/user/user.module.ts
+++ js-team6/src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule,TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService]
 })
